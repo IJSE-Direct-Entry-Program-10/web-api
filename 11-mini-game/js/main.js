f2c1518afcd2ea8f73417c0ff064cb0cb4fad2d2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-05-21 13:26:17 +0530

    Add background and fullscreen support

--- 11-mini-game/js/main.js
+++ 11-mini-game/js/main.js
@@ -1,6 +1,22 @@
+/* Load all the images at the startup */
+for(let i = 1; i <= 12; i++){
+    const image = new Image();
+    image.src  = `img/Jump (${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Idle (${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Walk (${i}).png`;
+}
+
 const boxElm = document.createElement('div');
 boxElm.classList.add('box');
-document.body.append(boxElm);
+document.getElementById('background').append(boxElm);
+
+document.body.addEventListener('click', ()=> document.body.requestFullscreen());
 
 let jump = false;
 let run = false;
@@ -10,9 +26,11 @@ document.body.addEventListener('keydown', (eventData)=> {
     if (eventData.code === 'Space'){
         jump = true;
     }else if (eventData.code === 'ArrowRight'){
+        boxElm.style.transform = 'rotateY(0deg)'
         run = true;
         dx = 2;
     }else if (eventData.code === 'ArrowLeft'){
+        boxElm.style.transform = 'rotateY(180deg)';
         run = true;
         dx = -2;
     }
@@ -44,14 +62,26 @@ function doRun(){
     let x = boxElm.offsetLeft + dx;
     if ((x + boxElm.offsetWidth)> innerWidth) {
         x = innerWidth - boxElm.offsetWidth;
-    }else if (x <= 0) x = 0;
+    }else if (x <= -250) x = -250;
     boxElm.style.left = `${x}px`;
 }
 
 let i = 1;
 function drawIdle(){
     boxElm.style.backgroundImage = `url('img/Idle (${i++}).png')`;
-    if(i === 11) i = 1;
+    if(i === 10) i = 1;
+}
+
+let k = 1;
+function drawJump(){
+    boxElm.style.backgroundImage = `url('img/Jump (${k++}).png')`;
+    if(k === 12) k = 1;
+}
+
+let j = 1;
+function drawRun(){
+    boxElm.style.backgroundImage = `url('img/Walk (${j++}).png')`;
+    if(j === 10) j = 1;
 }
 
 setInterval(()=> {
@@ -63,4 +93,12 @@ setInterval(()=> {
     }
 }, 5);
 
-setInterval(()=> drawIdle(), (1000/20));
+setInterval(()=> {
+    if (!jump && !run){
+        drawIdle();
+    }else if (jump){
+        drawJump();
+    }else if (!jump && run){
+        drawRun();
+    }
+} , (1000/20));
